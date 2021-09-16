fd710de12e763ec956d0ac91525f101f8a17eaac
buthed <temaTihonov@yandex.ru>
2021-09-16 22:53:11 +0300

    RadioButtonGroup with Toasts

--- app/src/main/java/com/example/materialdesign/view/settings/SettingsFragment.kt
+++ app/src/main/java/com/example/materialdesign/view/settings/SettingsFragment.kt
@@ -4,8 +4,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.RadioButton
+import android.widget.RadioGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+import com.example.materialdesign.R
 import com.example.materialdesign.databinding.FragmentSettingsBinding
 
 class SettingsFragment:Fragment() {
@@ -14,6 +17,8 @@ class SettingsFragment:Fragment() {
         get() {
             return _bindong!!
         }
+
+    //var bg: String = "default"
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -37,10 +42,30 @@ class SettingsFragment:Fragment() {
         binding.chipWithClose.setOnCloseIconClickListener {
             Toast.makeText(context,"Click on chipWithClose",Toast.LENGTH_SHORT).show()
         }
+        binding.radioGroup.setOnCheckedChangeListener { _, checkedId ->
+            when(checkedId){
+                R.id.rb1 -> Toast.makeText(context,"Cosmos",Toast.LENGTH_SHORT).show()
+                R.id.rb2 -> Toast.makeText(context,"Moon",Toast.LENGTH_SHORT).show()
+                R.id.rb3 -> Toast.makeText(context,"Mars",Toast.LENGTH_SHORT).show()
+            }
+        }
     }
+
+/*
+bg = when(checkedId){
+binding.rb1 -> "1"
+binding.rb2 -> binding.settingsFr.background = "@drawable/bg_cosmos"
+binding.rb3 -> binding.settingsFr.background = "@drawable/bg_cosmos"
+else -> binding.settingsFr.background = "@drawable/bg_cosmos"
+}
+val a: String = binding.rb1.toString()
+binding.settingTitle.text = "option "+i+" is selected"
+*/
+
+
+
+
     companion object {
         fun newInstance() = SettingsFragment()
     }
-
-
 }
