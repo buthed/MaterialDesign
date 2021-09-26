314ff4940ab84471868aee5278458d54ae2b6124
buthed <temaTihonov@yandex.ru>
2021-09-26 15:56:56 +0300

    Fix remarks from Lesson_3: Several api requests

--- app/src/main/java/com/example/materialdesign/viewmodel/PODData.kt
+++ app/src/main/java/com/example/materialdesign/viewmodel/PODData.kt
@@ -4,6 +4,7 @@ import com.example.materialdesign.repository.PODServerResponseData
 
 sealed class PODData{
     data class Success(val serverResponseData: PODServerResponseData) : PODData()
+    data class SuccessMars(val serverResponseData: MarsPhotosServerResponseData) : PODData()
     data class Error(val error: Throwable) : PODData()
     data class Loading(val progress: Int?) : PODData()
 }
