587de03048f5fdba30d02ff1fca9c67682ff15cc
buthed <temaTihonov@yandex.ru>
2021-09-28 23:17:08 +0200

    Start Lesson 5

--- app/src/main/java/com/example/materialdesign/api/EarthFragment.kt
+++ app/src/main/java/com/example/materialdesign/api/EarthFragment.kt
@@ -6,14 +6,28 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.materialdesign.R
+import com.example.materialdesign.databinding.FragmentEarthBinding
+import com.example.materialdesign.databinding.FragmentMarsBinding
 
 class EarthFragment: Fragment() {
+
+    private var _binding: FragmentEarthBinding? = null
+    val binding: FragmentEarthBinding
+        get() {
+            return _binding!!
+        }
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_earth, container, false)
+        _binding = FragmentEarthBinding.inflate(inflater)
+        return binding.root
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 }
